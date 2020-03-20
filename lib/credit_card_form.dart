3b4b907718469c06835c14950b9d037d76257229
CarreBenjamin <42135497+CarreBenjamin@users.noreply.github.com>
2020-03-20 10:46:38 +0100

    Update credit_card_form.dart

--- lib/credit_card_form.dart
+++ lib/credit_card_form.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
-
+import 'package:baguette_express_client/src/Shared/buttons.dart';
 import 'credit_card_model.dart';
+import 'package:baguette_express_client/src/Shared/colors.dart';
+
 
 class CreditCardForm extends StatefulWidget {
   const CreditCardForm({
@@ -12,6 +14,8 @@ class CreditCardForm extends StatefulWidget {
     this.cardHolderName,
     this.cvvCode,
     @required this.onCreditCardModelChange,
+    this.onConfirmPayment,
+    this.onCancelPayment,
     this.themeColor,
     this.textColor = Colors.black,
     this.cursorColor,
@@ -25,12 +29,17 @@ class CreditCardForm extends StatefulWidget {
   final Color themeColor;
   final Color textColor;
   final Color cursorColor;
+  final VoidCallback  onConfirmPayment;
+  final VoidCallback onCancelPayment;
 
   @override
   _CreditCardFormState createState() => _CreditCardFormState();
 }
 
 class _CreditCardFormState extends State<CreditCardForm> {
+
+  final _formKey = GlobalKey<FormState>();
+
   String cardNumber;
   String expiryDate;
   String cardHolderName;
@@ -124,6 +133,7 @@ class _CreditCardFormState extends State<CreditCardForm> {
         primaryColorDark: themeColor,
       ),
       child: Form(
+        key: _formKey,
         child: Column(
           children: <Widget>[
             Container(
@@ -131,13 +141,20 @@ class _CreditCardFormState extends State<CreditCardForm> {
               margin: const EdgeInsets.only(left: 16, top: 16, right: 16),
               child: TextFormField(
                 controller: _cardNumberController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Complétez votre numéro de carte";
+                  } else {
+                    return null;
+                  }
+                },
                 cursorColor: widget.cursorColor ?? themeColor,
                 style: TextStyle(
                   color: widget.textColor,
                 ),
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Card number',
+                  labelText: 'Numéro de carte',
                   hintText: 'xxxx xxxx xxxx xxxx',
                 ),
                 keyboardType: TextInputType.number,
@@ -149,14 +166,21 @@ class _CreditCardFormState extends State<CreditCardForm> {
               margin: const EdgeInsets.only(left: 16, top: 8, right: 16),
               child: TextFormField(
                 controller: _expiryDateController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Complétez votre date d'expiration";
+                  } else {
+                    return null;
+                  }
+                },
                 cursorColor: widget.cursorColor ?? themeColor,
                 style: TextStyle(
                   color: widget.textColor,
                 ),
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: 'Expired Date',
-                    hintText: 'MM/YY'),
+                    labelText: "Date d'expiration",
+                    hintText: 'MM/AA'),
                 keyboardType: TextInputType.number,
                 textInputAction: TextInputAction.next,
               ),
@@ -164,11 +188,18 @@ class _CreditCardFormState extends State<CreditCardForm> {
             Container(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
               margin: const EdgeInsets.only(left: 16, top: 8, right: 16),
-              child: TextField(
+              child: TextFormField(
                 focusNode: cvvFocusNode,
                 controller: _cvvCodeController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Complétez votre CVV";
+                  } else {
+                    return null;
+                  }
+                },
                 cursorColor: widget.cursorColor ?? themeColor,
-                style: TextStyle(
+                     style: TextStyle(
                   color: widget.textColor,
                 ),
                 decoration: InputDecoration(
@@ -190,19 +221,41 @@ class _CreditCardFormState extends State<CreditCardForm> {
               margin: const EdgeInsets.only(left: 16, top: 8, right: 16),
               child: TextFormField(
                 controller: _cardHolderNameController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Complétez votre Nom et Prénom";
+                  } else {
+                    return null;
+                  }
+                },
                 cursorColor: widget.cursorColor ?? themeColor,
                 style: TextStyle(
                   color: widget.textColor,
                 ),
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: 'Card Holder',
+                  labelText: "Nom Prénom",
                 ),
                 keyboardType: TextInputType.text,
                 textInputAction: TextInputAction.next,
               ),
             ),
-          ],
+        FlatButton(
+          onPressed: (){
+              if(_formKey.currentState.validate()){
+            widget.onConfirmPayment(); }},
+          child: Text("Confirmer l'achat"),
+          textColor: white,
+          color: primaryColor,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        ),
+            FlatButton(
+              onPressed: (){widget.onCancelPayment();},
+              child: Text("Retour au panier"),
+              textColor: white,
+              color: primaryColor,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+            ),],
         ),
       ),
     );
